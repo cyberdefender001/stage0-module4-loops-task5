0ff81fcce6aa075c0eac4db92578802c5064f210
cyberdefender001 <101460837+cyberdefender001@users.noreply.github.com>
2022-09-17 14:59:10 +0500

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+        for (int i = 0; i < cathetusLength; i++) {
+                System.out.println();
+            for (int j = i + 1; j < cathetusLength; j++) {
+                System.out.print(8);
+            }
+        }
     }
 }
